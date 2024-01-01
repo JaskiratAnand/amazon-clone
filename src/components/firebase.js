1270eb3832d57d44fa4bfde31cad8539f6c807d4
JaskiratAnand <jaskiratanand01@gmail.com>
2024-01-01 21:35:49 +0530

    fixing firebase apis

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -4,13 +4,13 @@ import "firebase/compat/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDTlVBMPxSizPIVkbOgQXR7ZL6I2WnCPCc",
-  authDomain: "clone-afc2f.firebaseapp.com",
-  projectId: "clone-afc2f",
-  storageBucket: "clone-afc2f.appspot.com",
-  messagingSenderId: "351436460144",
-  appId: "1:351436460144:web:2b06c40b222042937c64b5",
-  measurementId: "G-WR1PB254EN"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_MEADUREMENT_ID
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
